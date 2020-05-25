d91dc2142e1db9a02717d722854a5c80d54176c4
singhshivam <singhshivam.172@gmail.com>
2020-05-25 18:38:04 -0500

    working with components

---
+++ src/components/header.js
@@ -0,0 +1,6 @@
+import React from "react"
+
+export default function Header(props) {
+    const { headerText } = props
+    return <h1>{headerText}</h1>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `teal` }}>
+      <Header headerText="About components"/>
+      <h1>Hello Gatsby!</h1>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
 }
