a7fd96d6402d37a3dedc070c4550ac5db9db5f72
singhshivam <singhshivam.172@gmail.com>
2020-05-25 19:12:58 -0500

    nested style layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
 import React from "react"
 import Header from "../components/header"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div style={{ color: `teal` }}>
-      <Link to="/contact/">Contact</Link>
-      <Link to="/about-css-modules/">About CSS Modules</Link>
-      <Header headerText="About components"/>
-      <h1>Hello Gatsby!</h1>
-      <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
-    </div>
+    <Layout>
+      <div style={{ color: `teal` }}>
+        <Header headerText="About components" />
+        <h1>Hello Gatsby!</h1>
+        <p>What a world.</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+      </div>
+    </Layout>
   )
 }
